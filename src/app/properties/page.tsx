2d942fc5b2e4e8e5199d6777b8c89e21b19ee275
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-10-26 22:34:48 +0300

    Make PropertyCard component dynamic

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -1,18 +1,20 @@
+import type { propertyProps } from "@/types/basicTypes";
 import properties from "@/data/properties.json";
 import PropertyCard from "@/components/PropertyCard";
 function PropertiesPage() {
+  const props: propertyProps[] = properties as propertyProps[];
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
           Recent Properties
         </h2>
-        {properties.length === 0 ? (
+        {props.length === 0 ? (
           <p>No property found</p>
-        ) : ( 
+        ) : (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <div>{property.name}</div>
+            {props.map((property) => (
+              <PropertyCard key={property._id} property={property} />
             ))}
           </div>
         )}
--- src/types/basicTypes.ts
+++ src/types/basicTypes.ts
@@ -2,6 +2,7 @@ import React from "react";
 export interface mainLayoutProps {
   children?: React.ReactNode;
 }
+
 export interface infoBoxProps {
   heading: string;
   backgroundColor?: string;
@@ -13,4 +14,42 @@ interface buttonInfoProps {
   text: string;
   link: string;
   backgroundColor: string;
+}
+
+export interface propertyCardProps {
+  property: propertyProps;
+}
+export interface propertyProps {
+  _id: String | number;
+  owner: String;
+  name: String;
+  type: String;
+  description: String;
+  location: locationProps;
+  beds: 2;
+  baths: 1;
+  square_feet: 1500;
+  amenities: String[];
+  rates: ratesProps;
+  seller_info: sellerInfoProps;
+  images: String[];
+  is_featured: boolean;
+  createdAt: String;
+  updatedAt: String;
+}
+interface locationProps {
+  street: String;
+  city: String;
+  state: String;
+  zipcode: String;
+}
+interface ratesProps {
+  weekly?: number;
+  monthly?: number;
+  nightly?: number;
+}
+interface sellerInfoProps {
+  name: String;
+  email: String;
+  phone: String;
 }
