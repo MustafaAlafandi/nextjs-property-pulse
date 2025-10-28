23ce428ef559bb2ac0eb647b209894c2284a9d4e
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-10-28 20:57:40 +0300

    Add User and Property Schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperites from "@/components/HomeProperites";
-import connectDB from "@/config/database";
 async function HomePage() {
-  await connectDB();
   return (
     <>
       <Hero />
