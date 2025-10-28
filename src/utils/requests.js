2119ec7c60de99c39cf15443272e539003572b1b
mustafa alafandi <163916161MustafaAlafandi@users.noreply.github.com>
2025-10-28 23:33:57 +0300

    Put fetchProperites function in new file and export it

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,11 +1,16 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 export async function fetchProperties() {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+    if(!apiDomain){
+        return [];
+    }
+    try {
+    const res = await fetch(`${apiDomain}/properties`);
     if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
     return res.json();
   } catch (err) {
     console.log(err);
+    return [];
   }
 }
